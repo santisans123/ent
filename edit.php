8ae946b7f6448e76a4707c0526ddad8695e36ddf
santi <santitok21@gmail.com>
2020-10-08 10:14:12 +0700

    push ke 3

--- edit.php
+++ edit.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tambah Data</title>
+    <title>Tambah Data Berita</title>
 </head>
 <body>
 <?php
