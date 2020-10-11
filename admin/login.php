d8b62d31ed5bdfa0afbd4c90154882440c7f99e7
santi <santitok21@gmail.com>
2020-10-11 10:15:24 +0700

    revisi

--- admin/login.php
+++ admin/login.php
@@ -2,56 +2,40 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Jekyll v3.8.5">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Login Form</title>
- 
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/sign-in/">
- 
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
- 
- 
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-      }
- 
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
-    <!-- Custom styles for this template -->
+    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+
   </head>
-  <body class="text-center">
-    <form class="form-signin" method="post" action="ceklogin.php">
-  <h1 class="h3 mb-3 font-weight-normal">PLEASE SIGN IN</h1>
-  <label for="username" class="sr-only">Username</label>
-  <input type="text" id="username" class="form-control" placeholder="Username" name="username" required autofocus>
+  <body>
+  <form class="form-signin" method="post" action="ceklogin.php">
+  <div class="login">
+     <div class="avatar">
+     <i class="fa fa-user"></i>
+     </div>
+
+        <center><h1 class="h3 mb-3 font-weight-normal">SIGN IN</h1></center>
+
+        <div class="box-login">
+        <i class="fas fa-user"></i>
+        <input type="text" id="username" class="form-control" placeholder="Username" name="username" required autofocus>
+        </div>
+
+        <div class="box-login">
+        <i class="fas fa-lock"></i>
+        <input type="password" id="password" class="form-control" placeholder="Password" name="password" required>
+        </div>
+        
+        <label>
+        <div class="a">
+        <input type="checkbox" value="remember-me" name="remember"> Remember Password </div>
+        </label>
+  
   <br>
-  <label for="password" class="sr-only">Password</label>
-  <input type="password" id="password" class="form-control" placeholder="Password" name="password" required>
-  </br>
-  <div class="checkbox mb-3">
-    <label>
-      <input type="checkbox" value="remember-me" name="remember"> Remember Password
-    </label>
-  </div>
-  <br>
-  <button class="btn btn-lg btn-primary btn-block" type="submit" name="login">Sign in</button>
-  </br>
-  <p class="mt-5 mb-3 text-muted">Portal Website &copy; PENS</p>
+  <button class="btn-login" type="submit" name="login">Sign in</button>
 </form>
+<center><div class="a">Portal Website &copy; PENS</div></center>
+</div>
 </body>
-<script>
-</script>
 </html>
